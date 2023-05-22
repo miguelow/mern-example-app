4aec24e048073545128eb304206434f0315f5f70
miguelow <miguel.owd@gmail.com>
2023-05-22 19:28:22 +0200

    first express routes w parameters

--- section8-express/server.js
+++ section8-express/server.js
@@ -4,6 +4,34 @@ const app = express();
 
 const PORT = 3000
 
+let friends = [
+    {
+        id: 0,
+        name: 'Roci'
+    },
+    {
+        id: 1,
+        name: 'Miguel'
+    },
+]
+
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
+})
+
+app.get('/friends', (req, res) => {
+    res.json(friends)
+})
+
+app.get('/friends/:friendId', (req, res) => {
+    //el id vuelve de la url como string que tenemos que pasar a un numero
+    const friendId = Number(req.params.friendId)
+    const friend = friends[friendId]
+    if (friend) {
+        res.json(friend)
+    }else{
+        res.status(404).json({
+            error: 'Friend not found'
+        })
+    }
 })
