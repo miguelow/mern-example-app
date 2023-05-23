ef8a394617cc732fe00b24e1ed762f75e4c67b81
Miguel <miguel.owd@gmail.com>
2023-05-23 15:14:19 +0200

    initial project structure and imported frontend preset

--- section8-express/server.js
+++ section8-express/server.js
@@ -25,7 +25,7 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
   res.render('index', {
-    title: 'Skying tryp 2018',
+    title: 'Skying trip 2018',
     caption: 'Lets go skiing!',
   });
 });
