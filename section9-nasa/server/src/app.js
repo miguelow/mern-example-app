f6fc88558e8ff64329fa85ed342f3955b319ce84
Miguel <miguel.owd@gmail.com>
2023-05-25 10:34:02 +0200

    created optimized build to serve frontend from backed port

--- section9-nasa/server/src/app.js
+++ section9-nasa/server/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 var cors = require('cors')
+const path = require('path')
 
 const planetsRouter = require('../routes/planets/planets.router')
 
@@ -8,5 +9,9 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 app.use(planetsRouter );
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+  });
 
 module.exports = app;
