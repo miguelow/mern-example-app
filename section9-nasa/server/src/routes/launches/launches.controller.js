766b693fe0462225180d2691d405268881b7b845
Miguel <miguel.owd@gmail.com>
2023-05-29 15:49:30 +0200

    update launches

--- section9-nasa/server/src/routes/launches/launches.controller.js
+++ section9-nasa/server/src/routes/launches/launches.controller.js
@@ -1,4 +1,4 @@
-const {getAllLaunches, addNewLaunch} = require('../../models/launches.model')
+const {getAllLaunches, addNewLaunch, existsLaunchWithId, abortLaunchById} = require('../../models/launches.model')
 
 //Al usar la nomenclatura http... sabemos que esa funcion devuelve una response
 function httpGetAllLaunches(req, res){
@@ -24,7 +24,30 @@ function httpAddNewLaunch(req, res){
     //return so we only set the response once per controller function
 }
 
+function httpAbortLaunch(req, res) {
+    const launchId = Number(req.params.id);
+  
+    const existsLaunch = existsLaunchWithId(launchId);
+    if (!existsLaunch) {
+      return res.status(404).json({
+        error: 'Launch not found',
+      });
+    }
+  
+    const aborted = abortLaunchById(launchId);
+    if (!aborted) {
+      return res.status(400).json({
+        error: 'Launch not aborted',
+      });
+    }
+  
+    return res.status(200).json({
+      ok: true,
+    });
+  }
+
 module.exports = {
     httpGetAllLaunches,
     httpAddNewLaunch,
+    httpAbortLaunch,
 }
