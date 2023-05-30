0c3fef797483e1674a325cc24270ad0c346a28ca
Miguel <miguel.owd@gmail.com>
2023-05-30 10:26:30 +0200

    testing succes cases in launches

--- section9-nasa/server/src/routes/launches/launches.test.js
+++ section9-nasa/server/src/routes/launches/launches.test.js
@@ -1,13 +1,43 @@
+const request = require('supertest')
+const app = require('../../app')
+
 describe("Test GET /launches", () => {
-    test('It should respond with 200 success', () => {
-        const response = 200
-        expect(response).toBe(200)
+    test('It should respond with 200 success', async () => {
+        const response = await request(app)
+            .get('/launches')
+            .expect('Content-Type', /json/)
+            .expect(200)
     })
 })
 
 describe("Test POST /launches", () => {
-    test('It should respond with 200 success', () => {
-            
+
+    const launchData = {
+        mission: 'test',
+        rocket: 'rocketname',
+        target: 'madrid',
+        launchDate: "March 1, 2024"
+    }
+
+    const launchDataWithoutDate = {
+        mission: 'test',
+        rocket: 'rocketname',
+        target:'madrid',
+    }
+
+    test('It should respond with 201 created', async () => {
+        const response = await request(app)
+        .post('/launches')
+        .send(launchData)
+        .expect('Content-Type', /json/)
+        .expect(201)
+        
+        const requestDate = new Date(launchData.launchDate).valueOf
+        const responseDate = new Date(response.body.launchDate).valueOf
+        expect(responseDate).toBe(requestDate)
+
+        //To check the body use hest assertions
+        expect(response.body).toMatchObject(launchDataWithoutDate)
     })
     test('It should catch missing requires properties', () => {
         
