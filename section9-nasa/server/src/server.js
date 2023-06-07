d0b8e5620503c677a01314571322a9554c30e5ec
Miguel <miguel.owd@gmail.com>
2023-06-07 11:30:35 +0200

    updated jest server tests

--- section9-nasa/server/src/server.js
+++ section9-nasa/server/src/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
-const mongoose = require('mongoose');
 
 const app = require ('./app');
+const {mongoConnect} = require('./services/mongo')
 const {loadPLanetsData} = require('./models/planets.model')
 require('dotenv').config();
 
@@ -9,18 +9,8 @@ const server = http.createServer(app);
 
 const PORT = process.env.PORT
 
-const  MONGO_URL = process.env.MONGO_URL
-
-mongoose.connection.once('connected', () => {
-    console.log('Connected to MongoDB')
-})
-
-mongoose.connection.on('error', (err) => {
-    console.log('Error connecting to MongoDB', err)
-})
-
 async function startServer(){
-    await mongoose.connect(MONGO_URL)
+    await mongoConnect()
     //Load data on startup
     await loadPLanetsData()
     server.listen(PORT, () => {
