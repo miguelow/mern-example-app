ae16e3831160df5350aa9e2e2ffa8d85e59aa06a
Miguel <miguel.owd@gmail.com>
2023-07-12 12:00:02 +0200

    server bugfix

--- security-example/server.js
+++ security-example/server.js
@@ -1,3 +1,4 @@
+const https = require('https');
 const path = require('path');
 const express = require('express');
 
@@ -9,6 +10,13 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
 
-app.listen(PORT, () => {
+app.get('/secret', (req, res) => {
+    return res.send('Shh')
+})
+
+https.createServer({
+    cert: '',
+    key: '',
+}).listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
 })
