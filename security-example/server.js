387b7e39156dd84d73e83260ea0f000d8e2f7613
Miguel <miguel.owd@gmail.com>
2023-07-24 16:15:32 +0200

    installed passportjs and started creating middleware

--- security-example/server.js
+++ security-example/server.js
@@ -3,14 +3,72 @@ const path = require('path');
 const https = require('https');
 const express = require('express');
 const helmet = require('helmet');
+const passport = require('passport');
+const { Strategy } = require('passport-google-oauth20')
+
+require('dotenv').config()
 
 const PORT = 3000;
+const config = {
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET
+}
+
+const AUTH_OPTIONS = {
+  callbackURL: '/auth/google/callback',
+  clientID: config.CLIENT_ID,
+  clientSecret: config.CLIENT_SECRET
+};
+
+function verifyCallback(accessToken, refreshToken, profile, done){
+  console.log('Google profile', profile);
+  done(null, profile)
+};
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback))
 
 const app = express();
 
-app.use(helmet());
+//Midleware chain
+app.use(helmet()); //Secures endpoints
+app.use(passport.initialize());
 
-app.get('/secret', (req, res) => {
+function checkLoggedIn(req, res, next) {
+  const isLoggedIn = true //TODO
+  if(!isLoggedIn){
+    return res.status(401).json({
+      error: 'You must log in'})
+  }
+  next();
+}
+
+app.get('/auth/google', 
+  passport.authenticate('google', {
+    scope: ['email'],
+    session: false,
+  })
+);
+
+app.get('/auth/google/callback', 
+  passport.authenticate('google', {
+    failureRedirect: '/failure',
+    successRedirect: '/',
+    session: false,
+  }), 
+  (req, res) => {
+    console.log('Google called us back')
+  }
+);
+
+app.get('/failure', function(req, res) {
+  return res.send('Failed logging in');
+})
+
+app.get('/auth/logout', function(req, res) {
+
+});
+
+app.get('/secret', checkLoggedIn,  (req, res) => {
   return res.send('Your personal secret value is 42!');
 });
 
